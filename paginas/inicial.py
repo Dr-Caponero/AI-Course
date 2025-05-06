8cdd836239e3ce48b66b199ac15793bfa0699b45
Jeff Caponero <72578904+Dr-Caponero@users.noreply.github.com>
2025-05-06 12:38:00 -0300

    Update inicial.py

--- paginas/inicial.py
+++ paginas/inicial.py
@@ -18,4 +18,26 @@ st.image(main_bg2)
 """ 
 **Bem-vindo ao Aplicativo de Aprendizado de Inteligência Artificial! ✨**
 
+
+Olá, pessoal! 👋
+
+Este é o **aplicativo online** que vamos usar na disciplina **Inteligência Artificial** do **IFBA 2025.1**. 
+
+Aqui, vamos reunir conteúdos e ferramentas que construiremos durante as aulas, e ele será o ponto central para **compartilhar materiais** e **novidades** da disciplina.
+
+Tudo que vocês precisarem estará disponível nessa página, então fiquem ligados! 
+
+Apps disponíveis até o momento:
+
+- 🐍 Instrutor de Python
+
+**Lembrem-se:** com tecnologia GPT, tudo pode estar errado, então usem com atenção. Qualquer coisa esquisita que acontecem, **relate!** ⚠️
+
+Se tiverem sugestões ou dúvidas, me chamem! Espero que tenham todos uma ótima experiência.
+
+Bora aprender! 💪
+
+Prof. Jeff  
+
 """
+
